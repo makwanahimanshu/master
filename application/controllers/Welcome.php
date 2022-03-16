911813ff79a419955217fa75d04dd35d4525c53e
makwanahimanshu <himanshumakwana3110@gmail.com>
2022-03-16 11:23:53 +0530

    add files

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	// public function index()
+	// {
+	// 	$this->load->view('welcome_message');
+	// }
+
+	// public function basic_forms()
+	// {
+	// 	$this->load->view('basic_forms');
+	// }
+
+	public function index()
+	{
+		$this->load->view('index');
+	}
+
+	// public function basic_tables()
+	// {
+	// 	$this->load->view('basic_tables');
+	// }
+
+	public function login()
+	{
+		$this->load->view('login');
+	}
+
+	public function register()
+	{
+		$this->load->view('register');
+	}
+}
---
+++ application/views/index2.php
@@ -0,0 +1,28 @@
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<title>403 Forbidden</title>
+</head>
+<body>
+
+<p>Directory access is forbidden.</p>
+
+</body>
+
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Material Dash</title>
+</head>
+<body>
+  <script>
+    // similar behavior as an HTTP redirect
+    window.location.replace("demo/index.html");
+  </script>
+</body>
+</html>
